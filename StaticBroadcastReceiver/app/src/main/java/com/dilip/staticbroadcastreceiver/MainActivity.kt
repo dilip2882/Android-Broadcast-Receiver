11c3aafbc8d16c57d5bbaf6d14836150eb682e00
Dilip Patel <pateldilip2902@gmail.com>
2024-07-01 11:35:37 +0530

    StaticBroadcastReceiver

---
+++ StaticBroadcastReceiver/app/src/main/java/com/dilip/staticbroadcastreceiver/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dilip.staticbroadcastreceiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+/*
+        // Register receiver dynamically
+        val filter = IntentFilter(Intent.ACTION_BOOT_COMPLETED)
+        registerReceiver(ExampleBroadcast(), filter)
+*/
+
+    }
+}
