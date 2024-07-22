8a7e5402627758012baf81e4f1e72b397d87e7c0
Dilip Patel <pateldilip2902@gmail.com>
2024-07-22 15:32:25 +0530

    explicit

--- (Java)ExplicitBroadcastSender/app/src/main/java/com/dilip/javaexplicitbroadcastsender/MainActivity.java
+++ (Java)ExplicitBroadcastSender/app/src/main/java/com/dilip/javaexplicitbroadcastsender/MainActivity.java
@@ -2,11 +2,15 @@ package com.dilip.javaexplicitbroadcastsender;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,14 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendBroadcast(View view) {
 
+        Intent intent = new Intent("com.dilip.javaexplicitbroadcastsender.ACTION_SEND");
+
+//        intent.setPackage("com.dilip.javaexplicitbroadcastreceiver");
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> resolveInfos = packageManager.queryBroadcastReceivers(intent, 0);
+
+        for (ResolveInfo info : resolveInfos) {
+            ComponentName componentName = new
+                    ComponentName(info.activityInfo.packageName, info.activityInfo.name);
+            intent.setComponent(componentName);
+            sendBroadcast(intent);
+        }
+
 //        Intent intent = new Intent(this, MyBroadcastReceiver.class);
 //        intent.setClass(this, MyBroadcastReceiver.class);
 
-        Intent intent = new Intent();
-        ComponentName componentName = new ComponentName("com.dilip.javaexplicitbroadcastreceiver", "com.dilip.javaexplicitbroadcastreceiver.MyDemoReceiver");
-        intent.setComponent(componentName);
+//        Intent intent = new Intent();
+//        ComponentName componentName = new ComponentName("com.dilip.javaexplicitbroadcastreceiver", "com.dilip.javaexplicitbroadcastreceiver.MyDemoReceiver");
+//        intent.setComponent(componentName);
 
-        sendBroadcast(intent);
+//        sendBroadcast(intent);
     }
 
 }
